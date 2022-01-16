7abea2ab5e49810af8158c6f545775e0bab4379d
Maciej Olejnik <24421260+maciekole@users.noreply.github.com>
2022-01-16 16:36:40 +0100

    Updates

---
+++ precache-manifest.ba79358a648bb3c772b435fdd68ee57f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb55235903bfbabcf629d8aaec5bb81",
+    "url": "/react_tutorial/index.html"
+  },
+  {
+    "revision": "b92c345276bce7e57464",
+    "url": "/react_tutorial/static/css/main.dc26dd88.chunk.css"
+  },
+  {
+    "revision": "42d8e8859104679ce821",
+    "url": "/react_tutorial/static/js/2.bc0e5e72.chunk.js"
+  },
+  {
+    "revision": "77a17ceacf3be5f46709",
+    "url": "/react_tutorial/static/js/3.061514ed.chunk.js"
+  },
+  {
+    "revision": "b92c345276bce7e57464",
+    "url": "/react_tutorial/static/js/main.e14e907d.chunk.js"
+  },
+  {
+    "revision": "1bb1508d1e66794c1b93",
+    "url": "/react_tutorial/static/js/runtime~main.40ef66e5.js"
+  }
+]);
